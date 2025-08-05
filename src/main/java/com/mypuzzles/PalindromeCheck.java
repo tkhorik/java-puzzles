a7858afe64a200f90d1d6deb21ae17f1bd52f725
Tkhorik Dmitry <marvelmd@gmail.com>
2025-08-05 11:50:13 +0300

    Palindrome check added

--- src/main/java/com/mypuzzles/PalindromeCheck.java
+++ src/main/java/com/mypuzzles/PalindromeCheck.java
@@ -1,24 +1,38 @@
 package com.mypuzzles;
 
 public class PalindromeCheck {
-    // Method to determine if the input string is a palindrome
-    public static boolean isPalindrome(String str) {
-        String reverseStr = "";
-        int len = str.length();
-        // Reverse the string using a for loop
-        for (int i = len - 1; i >= 0; i--) {
-            reverseStr += str.charAt(i);
-        }
-        // Compare ignoring case differences
-        return str.equalsIgnoreCase(reverseStr);
-    }
+//    // Method to determine if the input string is a palindrome
+//    public static boolean isPalindrome(String str) {
+//        String reverseStr = "";
+//        int len = str.length();
+//        // Reverse the string using a for loop
+//        for (int i = len - 1; i >= 0; i--) {
+//            reverseStr += str.charAt(i);
+//        }
+//        // Compare ignoring case differences
+//        return str.equalsIgnoreCase(reverseStr);
+//    }
 
     public static void main(String[] args) {
-        String testStr = "Radar";
-        if(isPalindrome(testStr)) {
+        String testStr = "racecar";
+        if (isPalindrome(testStr)) {
             System.out.println(testStr + " is a palindrome.");
         } else {
             System.out.println(testStr + " is not a palindrome.");
         }
+    }
+
+    private static boolean isPalindrome(String testStr) {
+        int left = 0;
+        int right = testStr.length() - 1;
+        while (left < right) {
+            if (testStr.charAt(left) != testStr.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+
+        }
+        return true;
     }
 }
